6e605b5eb93bcb70b853d53883ca38820136a87c
Jonathan Tiede <skysoldiermars@gmail.com>
2018-03-21 21:45:32 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,29 +4,29 @@ require 'nokogiri'
 
 class Scraper
 
-  html = open('https://www.bodybuilding.com/exercises')
-  doc = Nokogiri::HTML(html)
-  list = doc.css('.exercise-list li')
-  body_parts = []
-  links = doc.css('.exercise-list li a')
+  def get_body_parts
+    html = open('https://www.bodybuilding.com/exercises')
+    doc = Nokogiri::HTML(html)
+    list = doc.css('.exercise-list li')
+    body_parts = []
+    links = doc.css('.exercise-list li a')
+    list.each do |element|
+      hash = {}
+      part = element.text
+      hash[:body_part] = part
+      hash[:url] = 'https://www.bodybuilding.com' + element.css('a')[0]['href']
+      body_parts << hash
+    end
 
-  list.each do |element|
-    hash = {}
-    part = element.text
-    hash[:body_part] = part
-    hash[:url] = 'https://www.bodybuilding.com' + element.css('a')[0]['href']
-    body_parts << hash
-end
+    # i = 1
+    # body_parts.each do |element|
+    #   puts element[:body_part]
+    #   i += 1
+    # 
+    # end
 
-  i = 1
-  body_parts.each do |element|
-    puts element[:body_part]
-    i += 1
+
+
 
   end
-
-
-
-
-
 end
